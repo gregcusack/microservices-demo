fa837bb3de47a39b3a9789b4012371984d58c173
triplewy <triplewy@gmail.com>
2020-03-16 20:55:08 -0400

    Guess I don't need vendor

--- src/faultservice/cmd/client/main.go
+++ src/faultservice/cmd/client/main.go
@@ -1,9 +1,46 @@
 package main
 
-func init() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/triplewy/microservices-demo/src/faultservice"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+)
 
+var (
+	addr string
+)
+
+func init() {
+	flag.StringVar(&addr, "addr", "", "address of fault service")
 }
 
 func main() {
+	flag.Parse()
 
+	cc, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := faultservice.NewFaultServiceClient(cc)
+
+	server, err := client.Experiment(context.Background(), &faultservice.EmptyMsg{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		msg, err := server.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Println(msg.GetInfo())
+	}
 }
