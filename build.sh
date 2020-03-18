da58adc5442a806f9d97616cb67a6cbebae2b7b0
triplewy <triplewy@gmail.com>
2020-03-17 21:28:14 -0400

    Removed some files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ./src
-for f in *
-do
-    echo "Building Docker: $f..."
-    # take action on each file. $f store current file name
-    docker build $f -t $f
-done
--- load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ./src
-for f in *
-do
-    echo "Loading: $f..."
-    # take action on each file. $f store current file name
-    kind load docker-image $f
-done
