b7056af4b5aa0fef9da2e7ae2244e70e8e50554f
Alexander Yu <ayu9@cs.brown.edu>
2020-03-22 23:37:05 -0400

    Fixed some yaml

--- envoy/start_envoy.sh
+++ envoy/start_envoy.sh
@@ -2,7 +2,9 @@
 docker stop envoy
 docker rm envoy
 docker build . -t envoy:grpc
-docker run -d --name envoy \
+docker run -d \
+	--restart=always \
+	--name envoy \
 	-p [::]:80:80 \
 	-p [::]:5000:5000 \
 	envoy:grpc
