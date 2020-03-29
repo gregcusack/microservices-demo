a577aa4539347f58a3ef42588d40c3c0d88317f5
triplewy <triplewy@gmail.com>
2020-03-28 21:29:35 -0400

    [Fix] Remove health check from dummy email service

--- src/emailservice/email_server.py
+++ src/emailservice/email_server.py
@@ -45,62 +45,7 @@ env = Environment(
 template = env.get_template('confirmation.html')
 
 
-class BaseEmailService(demo_pb2_grpc.EmailServiceServicer):
-    def Check(self, request, context):
-        return health_pb2.HealthCheckResponse(
-            status=health_pb2.HealthCheckResponse.SERVING)
-
-
-class EmailService(BaseEmailService):
-    def __init__(self):
-        raise Exception('cloud mail client not implemented')
-        super().__init__()
-
-    @staticmethod
-    def send_email(client, email_address, content):
-        response = client.send_message(
-            sender=client.sender_path(project_id, region, sender_id),
-            envelope_from_authority='',
-            header_from_authority='',
-            envelope_from_address=from_address,
-            simple_message={
-                "from": {
-                    "address_spec": from_address,
-                },
-                "to": [{
-                    "address_spec": email_address
-                }],
-                "subject": "Your Confirmation Email",
-                "html_body": content
-            }
-        )
-        logger.info("Message sent: {}".format(response.rfc822_message_id))
-
-    def SendOrderConfirmation(self, request, context):
-        email = request.email
-        order = request.order
-
-        try:
-            confirmation = template.render(order=order)
-        except TemplateError as err:
-            context.set_details(
-                "An error occurred when preparing the confirmation mail.")
-            logger.error(err.message)
-            context.set_code(grpc.StatusCode.INTERNAL)
-            return demo_pb2.Empty()
-
-        try:
-            EmailService.send_email(self.client, email, confirmation)
-        except GoogleAPICallError as err:
-            context.set_details("An error occurred when sending the email.")
-            print(err.message)
-            context.set_code(grpc.StatusCode.INTERNAL)
-            return demo_pb2.Empty()
-
-        return demo_pb2.Empty()
-
-
-class DummyEmailService(BaseEmailService):
+class DummyEmailService():
     def SendOrderConfirmation(self, request, context):
         logger.info('A request to send order confirmation email to {} has been received.'.format(
             request.email))
